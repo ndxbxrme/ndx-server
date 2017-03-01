e1adce4fab4db9662ddff60658b193c79968ec07
ndxbxrme <lewis_the_cat@hotmail.com>
2017-03-01 10:34:29 +0000

    ndxdb bump

--- build/test/test.js
+++ build/test/test.js
@@ -1,30 +1,16 @@
 (function() {
   'use strict';
-  exports.databaseReady = function(test) {
-    var e, error, ndx;
-    test.expect(1);
-    try {
-      ndx = require('../index.js').config({
-        database: 'rb',
-        tables: ['users', 'tasks'],
-        port: 23000
-      }).controller(function(ndx) {
-        return ndx.database.on('ready', function() {
-          return console.log('database ready');
-        });
-      }).start();
-      test.ok(true, 'database ready');
-      return test.done();
-    } catch (error) {
-      e = error;
-      console.log(e);
-      return test.done();
-    }
-  };
+  var ndx;
 
-
-  /*
-   */
+  ndx = require('../index.js').config({
+    database: 'rb',
+    tables: ['users', 'tasks'],
+    port: 23000
+  }).controller(function(ndx) {
+    return ndx.database.on('ready', function() {
+      return console.log(ndx.database.getDb().users.data.length);
+    });
+  }).start();
 
 }).call(this);
 
